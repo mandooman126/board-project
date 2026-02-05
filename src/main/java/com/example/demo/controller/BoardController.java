cb9d2b273c229389fdeb8eed470e79a493c48364
mandooman126 <choisw1206@naver.com>
2026-02-05 14:48:25 +0900

    comment: 주석 추가

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -9,8 +9,7 @@ public class BoardController {
 	@GetMapping("/board")
 	public String board() {
 		
-		//commit test1 -> 내 로컬 dev 브랜치 -> origin 브랜치로 commit, push
-		//commit test2 -> 내 로컬 dev 브랜치 -> origin 브랜치로 commit, push 이후 main pull - main push 이후 merge
+		//dev에서 올리고 main에서 merge시키기 test
 		return "boardList";
 	}
 	
